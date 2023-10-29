8a023a89dbeedb694b6951d4b669a6f0ae514dac
nikita <lfifnik11.17.01.2006@mail.ru>
2023-10-29 16:11:51 +0500

    HW_2

--- Homework1.py
+++ Homework1.py
@@ -5,46 +5,68 @@ class Student:
         self.gender = gender
         self.finished_courses = []
         self.courses_in_progress = []
-        self.grades = {}
+        self.grades_student = {}
  
     def add_courses(self, course_name):
         self.finished_courses.append(course_name)   
+
+    def rate_hw_lecturer(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+            if course in lecturer.grades_lecturer:
+                lecturer.grades_lecturer[course] += [grade]
+            else:
+                lecturer.grades_lecturer[course] = [grade]
+        else:
+            return 'Ошибка'
  
      
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
+        self.courses_attached = []       
+
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
         self.courses_attached = []
-        
-    def rate_hw(self, student, course, grade):
+        self.grades_lecturer = {}
+        self.grades_average_lecture = []
+
+
+class Reviewer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+    
+    def rate_hw_student(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
-            if course in student.grades:
-                student.grades[course] += [grade]
+            if course in student.grades_student:
+                student.grades_student[course] += [grade]
             else:
-                student.grades[course] = [grade]
+                student.grades_student[course] = [grade]
         else:
             return 'Ошибка'
 
 
-class Lecturer(Mentor):
-    pass
 
 
-class Reviewer(Mentor):
-    pass
-
-
-
-
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
+student_1 = Student('Ruoy', 'Eman', 'your_gender')
+student_1.courses_in_progress += ['Python']
  
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+reviewer_1 = Reviewer('Some', 'Buddy')
+reviewer_1.courses_attached += ['Python']
  
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+reviewer_1.rate_hw_student(student_1, 'Python', 10)
+reviewer_1.rate_hw_student(student_1, 'Python', 10)
+reviewer_1.rate_hw_student(student_1, 'Python', 10)
+
+lecturer_1 = Lecturer('Ivan', 'Ivanov')
+lecturer_1.courses_attached += ['Python']
+
+student_1.rate_hw_lecturer(lecturer_1, 'Python', 10)
+student_1.rate_hw_lecturer(lecturer_1, 'Python', 10)
+student_1.rate_hw_lecturer(lecturer_1, 'Python', 10)
  
-print(best_student.grades)
+print(f'Grades student {student_1.grades_student}')
+print(f'Grades lecturer {lecturer_1.grades_lecturer}')
